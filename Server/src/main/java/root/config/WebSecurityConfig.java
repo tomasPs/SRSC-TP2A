8b547edced60aa7677e6174b944967e6ea5515ba
tomasPs <t.pessanha@campus.fct.unl.pt>
2019-12-15 12:49:21 +0000

    update

--- Server/src/main/java/root/config/WebSecurityConfig.java
+++ Server/src/main/java/root/config/WebSecurityConfig.java
@@ -1,10 +1,9 @@
-package config;
+package root.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -12,14 +11,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/hello").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .x509()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+            .csrf().disable();
+//            .anyRequest().authenticated()
+//            .and()
+//            .x509()
+//            .and()
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
     }
 }
